58544062c4f4c1eec97412198ed4c73c05e3d241
adityaprasad365 <142519893+adityaprasad365@users.noreply.github.com>
2024-08-29 00:41:57 +0530

    Solution

---
+++ Countvalidstring and Invalid string.py
@@ -0,0 +1,20 @@
+def isValidString(strLis):
+    countValidString = 0
+    CountInvalidString = 0
+    for str in strLis:
+        temp = ""
+        for word in str.split():
+            temp = temp + word.strip()
+        if temp.isalpha():
+            countValidString = countValidString + 1
+        else:
+            CountInvalidString = CountInvalidString + 1
+    return [countValidString,CountInvalidString]
+
+strLis = []
+for i in range(int(input())):
+    strLis.append(input())
+
+valid,Invalid = isValidString(strLis)
+print("Count Of Valid String = ",valid)
+print("Count of Invalid String =",Invalid)
